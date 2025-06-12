a48cfb3f0073126428dc9f3e0d6434f5187ecd5c
Harry Bower <BowerHarry@users.noreply.github.com>
2025-06-12 01:20:01 +0100

    Send email for standing tickets only

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import { getTodayMinTicketPrice } from './scrapeTickets.js';
 import { sendEmail } from './sendEmail.js';
 
 const minPrice = await getTodayMinTicketPrice();
-if (minPrice !== null) {
+if (minPrice !== null && minPrice < 20) {
+  console.log(`There is a standing ticket available today for £${minPrice}!`);
   await sendEmail(minPrice);
 } else {
   console.log('No performances remaining today.');
