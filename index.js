a841400af79dc1a0bffcb2ae3572d56a693876e0
Harry Bower <BowerHarry@users.noreply.github.com>
2025-06-13 02:38:00 +0100

    Temporarily removed les mis index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import * as lesMis from './scrapeLesMiserablesTickets.js';
 import * as hamilton from './scrapeHamiltonTickets.js';
 import { sendEmail } from './sendEmail.js';
 
-const lesMisStandingTickets = await lesMis.areStandingTicketsAvailable();
+// const lesMisStandingTickets = await lesMis.areStandingTicketsAvailable();
 const hamiltonStandingTickets = await hamilton.areStandingTicketsAvailable();
 if (lesMisStandingTickets || hamiltonStandingTickets) {
   console.log(`There are standing tickets available today for Les Misérables: ${lesMisStandingTickets}, Hamilton: ${hamiltonStandingTickets}`);
-  // await sendEmail(lesMisStandingTickets, hamiltonStandingTickets);
+  await sendEmail(lesMisStandingTickets, hamiltonStandingTickets);
 } else {
   console.log('No standing tickets available.');
 }
