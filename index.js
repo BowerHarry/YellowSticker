eec14374c5f93264f02f1d3ed7af3b994cecb5fb
Harry Bower <BowerHarry@users.noreply.github.com>
2025-06-14 10:27:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import * as lesMis from './scrapeLesMiserablesTickets.js';
 import * as hamilton from './scrapeHamiltonTickets.js';
 import { sendEmail } from './sendEmail.js';
 
-// const lesMisStandingTickets = await lesMis.areStandingTicketsAvailable();
+const lesMisStandingTickets = false //await lesMis.areStandingTicketsAvailable();
 const hamiltonStandingTickets = await hamilton.areStandingTicketsAvailable();
 if (lesMisStandingTickets || hamiltonStandingTickets) {
   console.log(`There are standing tickets available today for Les Misérables: ${lesMisStandingTickets}, Hamilton: ${hamiltonStandingTickets}`);
